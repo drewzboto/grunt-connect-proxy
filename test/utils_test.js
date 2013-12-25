a02423d4a0a855d28cb592917c838b4869f4e1ec
Guilherme Rodrigues <gadr90@gmail.com>
2013-12-25 21:26:00 -0200

    Test array of contexts

--- test/utils_test.js
+++ test/utils_test.js
@@ -6,13 +6,27 @@ exports.utils_test = {
     done();
   },
   match_context_test: function(test) {
-    var singleContext, url, match;
-    test.expect(1);
+    var singleContext, arrayContext, url, match;
+    test.expect(4);
 
     singleContext = '/api';
     url = '/api/mock?suchrequest=1';
     match = utils.matchContext(singleContext, url);
     test.equal(match, true, 'should match single context with matching start');
+
+    url = '/notapi/mock?suchrequest=1';
+    match = utils.matchContext(singleContext, url);
+    test.equal(match, false, 'should not match single context with different start');
+
+    arrayContext = ['/api', '/superapi'];
+    url = '/superapi/mock?suchrequest=1';
+    match = utils.matchContext(arrayContext, url);
+    test.equal(match, true, 'should match array context with matching start');
+
+    singleContext = '/api';
+    url = '/api/mock/api';
+    match = utils.matchContext(singleContext, url);
+    test.equal(match, true, 'should match array context with matching start if same pattern is found later');
 
     test.done();
   }
